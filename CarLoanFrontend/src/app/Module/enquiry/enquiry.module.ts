8162a076f4876407080c2c3667239f57d0b41225
sneha lande <snehalande18@gmail.com>
2023-06-19 12:31:37 +0530

    commit

--- CarLoanFrontend/src/app/Module/enquiry/enquiry.module.ts
+++ CarLoanFrontend/src/app/Module/enquiry/enquiry.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SaveEnquiryComponent } from './save-enquiry/save-enquiry.component';
-import { ViewEnquiryComponent } from './view-enquiry/view-enquiry.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ViewEnquiryComponent } from '../re/view-enquiry/view-enquiry.component';
 
 
 
 @NgModule({
   declarations: [
     SaveEnquiryComponent,
-    ViewEnquiryComponent
+    
   ],
   imports: [
     CommonModule,
--- CarLoanFrontend/src/app/Module/enquiry/save-enquiry/save-enquiry.component.ts
+++ CarLoanFrontend/src/app/Module/enquiry/save-enquiry/save-enquiry.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { EnquiryServiceService } from 'src/app/shared/enquiry-service.service';
 
 @Component({
@@ -8,28 +7,89 @@ import { EnquiryServiceService } from 'src/app/shared/enquiry-service.service';
   templateUrl: './save-enquiry.component.html',
   styleUrls: ['./save-enquiry.component.css']
 })
-export class SaveEnquiryComponent  {
+export class SaveEnquiryComponent {
 
-constructor(private fb:FormBuilder,private enquiryservice:EnquiryServiceService){
+  saveForm: FormGroup; // Declare saveForm property
 
-}
-saveForm:FormGroup;
-ngOnInit(){
-  this.saveForm=this.fb.group({
-    'customerId':[],
-    'firstName' : [''],
-    'lastName' : [''],
-    'age' : [],
-    'emailId' : [''],
-    'mobileNumber' : [],
-    'adharNumber' : [],
-    'panCardNumber' : ['']
-  })
+  constructor(private fb: FormBuilder, private enquiryservice: EnquiryServiceService) {
+    this.saveForm = this.fb.group({
+      'firstName': new FormControl('', [Validators.required]),
+      'lastName' : new FormControl('', [Validators.required]),
+      'age' : new FormControl('', [Validators.required]),
+      'emailId' : new FormControl('', [Validators.required, Validators.email]),
+      'mobileNumber' : new FormControl('', [Validators.required, Validators.minLength(10)]),
+      'adharNumber' : new FormControl('', [Validators.required, Validators.minLength(12)]),
+      'panCardNumber' : new FormControl('', [Validators.required, Validators.minLength(15)])
+    });
+  }
+
+  saveUser() {
+    console.warn(this.saveForm.value);
+  }
+
+  get firstName() {
+    return this.saveForm.get('firstName');
+  }
+
+  get lastName()
+  {
+    return this.saveForm.get('lastName');
+  }
+
+  get age()
+  {
+    return this.saveForm.get('age');
+  }
+
+  get emailId()
+  {
+    return this.saveForm.get('emailId');
+  }
+  
+  get mobileNumber()
+  {
+    return this.saveForm.get('mobileNumber');
+  }
+
+  get adharNumber()
+  {
+    return this.saveForm.get('adharNumber');
+  }
+
+  get panCardNumber()
+  {
+    return this.saveForm.get('panCardNumber');
+  }
 }
 
-saveEnquiry(){
-this.enquiryservice.saveEnquiry(this.saveForm.value).subscribe((response)=>{
-  alert(response)
-})
-}
-}
+
+// saveForm:FormGroup;
+// ngOnInit(){
+//   this.saveForm=this.fb.group({
+//     'customerId':['', Validators.required],
+//     'firstName' : ['', Validators.required],
+//     'lastName' : ['', Validators.required],
+//     'age' : ['', Validators.required],
+//     'emailId' : ['', Validators.required, Validators.email],
+//     'mobileNumber' : ['', Validators.required, Validators.minLength(10)],
+//     'adharNumber' : ['', Validators.required, Validators.minLength(12)],
+//     'panCardNumber' : ['', Validators.required, Validators.minLength]
+//   })
+// }
+
+// saveUser()
+// {
+//   console.warn(this.saveForm.value);
+// }
+
+// get customerId()
+// {
+//   return this.saveForm.get(this.customerId)
+// }
+
+// saveEnquiry(){
+// this.enquiryservice.saveEnquiry(this.saveForm.value).subscribe((response)=>{
+//   alert(response)
+// })
+// }
+// }
